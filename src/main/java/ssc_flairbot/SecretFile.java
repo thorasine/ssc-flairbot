7c23dc15651436b5065d4b3ec302b339f61abe2b
Thorasine <zajakrisz@gmail.com>
2019-09-18 15:53:39 +0200

    Updating my super secret file

--- src/main/java/ssc_flairbot/SecretFile.java
+++ src/main/java/ssc_flairbot/SecretFile.java
@@ -4,7 +4,7 @@ import no.stelar7.api.l4j8.basic.*;
 
 public class SecretFile
 {
-    public static final String API_KEY            = "RGAPI-5b66deda-1e06-4503-bbb7-dd0e69f40051";
+    public static final String RIOT_API_KEY            = "RGAPI-5b66deda-1e06-4503-bbb7-dd0e69f40051";
     public static final String TOURNAMENT_API_KEY = "FAKE KEY";
-    public static final APICredentials CREDS      = new APICredentials(SecretFile.API_KEY, SecretFile.TOURNAMENT_API_KEY);
+    public static final APICredentials CREDS      = new APICredentials(SecretFile.RIOT_API_KEY, SecretFile.TOURNAMENT_API_KEY);
 }
