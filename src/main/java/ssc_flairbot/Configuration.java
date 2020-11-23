6879e4e81b154ebc2607d0941fba4bad91217883
thorasine <thorasine@users.noreply.github.com>
2020-11-23 14:16:38 +0100

    Update Configuration.java

--- src/main/java/ssc_flairbot/Configuration.java
+++ src/main/java/ssc_flairbot/Configuration.java
@@ -10,7 +10,7 @@ public class Configuration {
     //wipes the database, puts in sample data, and changes League API rate limits to match a development key's values
     public static final boolean IN_DEVELOPMENT_PHASE = true;
 
-    //percentage of the total capacity allowed to use for requests towards Riot's API for the updating the database
+    //percentage of the total capacity allowed to use for requests towards Riot's API for the periodic updating of database
     public static final double UPDATE_THRESHOLD = 0.8;
 
     //the amount of times the app tries to validate an user before setting them to "failed"
